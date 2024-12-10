77e867a88a98b29a9e44d6b5f68b7ba31d5c197d
sophia.jaeger <sojaeger@uos.de>
2024-12-10 13:23:45 +0100

    initialized dict and saves all wordds to dict

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from collections import defaultdict
 
 prefix = 'https://vm009.rz.uos.de/crawl/'
 
@@ -8,7 +9,7 @@ start_url = prefix+'index.html'
 agenda = [start_url]
 crawled_pages = []
 global results 
-results = {}
+results = defaultdict(lambda: defaultdict(int))  # a nested dictionary for words and their counts on urls
 
 while agenda:
     url = agenda.pop() # crawl the last page in the agenda
@@ -18,11 +19,10 @@ while agenda:
     print(r, r.encoding)
     if r.status_code == 200:
         soup = BeautifulSoup(r.content, 'html.parser')
-        for word in soup.get_text().split():
-            if word.lower() in results.keys():
-                results[word.lower()].append(url)
-            else:
-                results[word.lower()] = [url]
+        
+        for word in soup.get_text().lower().split():
+            results[word][url] += 1
+
         for anchor in soup.find_all('a'):
             # to do: only use page links that are on the prefix server
             href = anchor.get('href')
@@ -56,7 +56,10 @@ def search(word_list):
         if word in results.keys():
             urls
 
-print(results['the'])
+# Print results
+for word, urls in results.items():
+    for url, count in urls.items():
+        print(f"'{word}' found {count} times on {url}")
     
 
 
